69981b7085e11a296ce8ee2095e38046ec1179d5
Undo-all <undoall2@gmail.com>
2016-01-16 01:12:57 -0500

    add setup script that may or may not work

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+printf "Hi, and welcome to the interactive sshchan-functional setup script!\n"
+printf "If you've already installed sshchan before, this script would probably mess everything up, so probably just don't run this if you have.\n"
+printf "BEFORE you start this setup script, you should already have openssh-server installed, so if you don't, Ctrl+C now!"
+read 
+printf "Got it? Good!\n"
+printf "First we'll compile sshchan (note: you need ghc and a ton of packages to do this! If you don't have them, get them!)\n"
+ghc -O3 -threaded sshchan.hs
+printf "Done! Enter your chan's name: "
+read name
+cat setup.sql | sqlite3 "$name.db"
+printf "Enter the name of the user your sshchan-functional instance should run on: "
+read user
+printf "And what directory should sshchan be installed to?: "
+read dir
+groupadd anons
+useradd -d "$dir" -m -g anons -N -c 'sshchan user' "$user"
+printf "Now we'll set the password that will be used to connect to your sshchan instance.\n"
+passwd "$user"
+printf "Match User $user\n    ForceCommand ./sshchan"
+cp sshchan "$dir"
+mv "$name.db" "$dir"
+echo "$name" > "$dir/name.txt"
+printf "That should be it! Enjoy sshchan-functional!\n"
+
