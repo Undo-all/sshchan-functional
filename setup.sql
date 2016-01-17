92cae0d0217d0d52d67fbc6c745a8ed18de31e6a
Undo-all <undoall2@gmail.com>
2016-01-16 23:26:37 -0500

    NOT NULL EVERYTHING

--- setup.sql
+++ setup.sql
@@ -2,14 +2,14 @@ PRAGMA foreign_keys = ON;
 
 CREATE TABLE boards (
     board_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    board_name VARCHAR(255),
-    board_description VARCHAR(255)
+    board_name VARCHAR(255) NOT NULL,
+    board_description VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE posts (
     post_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    post_date DATE,
-    post_last_bumped DATETIME, 
+    post_date DATE NOT NULL,
+    post_last_bumped DATETIME NOT NULL,
     post_subject VARCHAR(255),
     post_by VARCHAR(255),
     post_content TEXT NOT NULL,
